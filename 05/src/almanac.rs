2903e7ae736ef4883b7e90879b85b791b160fde3
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-12-06 12:38:20 +0100

    05 part 1 solved

--- 05/src/almanac.rs
+++ 05/src/almanac.rs
@@ -1,46 +1,42 @@
 use regex::Regex;
-use std::{
-    collections::{HashMap, HashSet},
-    iter::zip,
-};
+use std::collections::HashSet;
 
-type Mapping = HashSet<(u32, u32, u32)>;
+#[derive(Debug, PartialEq)]
+struct Mapping(HashSet<(u64, u64, u64)>);
+
+impl Mapping {
+    fn new(set: HashSet<(u64, u64, u64)>) -> Self {
+        Mapping(set)
+    }
+
+    fn get(&self, key: u64) -> u64 {
+        self.0
+            .iter()
+            .find_map(|(value, key_inner, range)| {
+                if key >= *key_inner && key < key_inner + range {
+                    Some(key - key_inner + value)
+                } else {
+                    None
+                }
+            })
+            .unwrap_or(key)
+    }
+}
 
 #[derive(Debug)]
 pub struct Almanac {
-    seeds: HashSet<u32>,
-    seed_to_soil: HashMap<u32, u32>,
-    soil_to_fertilizer: HashMap<u32, u32>,
-    fertilizer_to_water: HashMap<u32, u32>,
-    water_to_light: HashMap<u32, u32>,
-    light_to_temperature: HashMap<u32, u32>,
-    temperature_to_humidity: HashMap<u32, u32>,
-    humidity_to_location: HashMap<u32, u32>,
+    seeds: HashSet<u64>,
+    mappings: Vec<Mapping>,
 }
 
 impl Almanac {
     pub fn new(input: &str) -> Option<Self> {
         let seeds = Self::parse_seeds(input)?;
-        let seed_to_soil = Self::parse_to_hashmap(input, "seed-to-soil")?;
-        let soil_to_fertilizer = Self::parse_to_hashmap(input, "soil-to-fertilizer")?;
-        let fertilizer_to_water = Self::parse_to_hashmap(input, "fertilizer-to-water")?;
-        let water_to_light = Self::parse_to_hashmap(input, "water-to-light")?;
-        let light_to_temperature = Self::parse_to_hashmap(input, "light-to-temperature")?;
-        let temperature_to_humidity = Self::parse_to_hashmap(input, "temperature-to-humidity")?;
-        let humidity_to_location = Self::parse_to_hashmap(input, "humidity-to-location")?;
-        Some(Self {
-            seeds,
-            seed_to_soil,
-            soil_to_fertilizer,
-            fertilizer_to_water,
-            water_to_light,
-            light_to_temperature,
-            temperature_to_humidity,
-            humidity_to_location,
-        })
+        let mappings = Self::parse_mappings(input)?;
+        Some(Self { seeds, mappings })
     }
 
-    fn parse_seeds(input: &str) -> Option<HashSet<u32>> {
+    fn parse_seeds(input: &str) -> Option<HashSet<u64>> {
         Some(
             Regex::new(r"(?m)seeds:([\s\d]+)\n")
                 .ok()?
@@ -48,13 +44,13 @@ impl Almanac {
                 .get(1)?
                 .as_str()
                 .split_ascii_whitespace()
-                .map(|str| str.parse::<u32>().unwrap())
-                .collect::<HashSet<u32>>(),
+                .map(|str| str.parse::<u64>().unwrap())
+                .collect::<HashSet<u64>>(),
         )
     }
 
     fn parse_mapping(input: &str, name: &str) -> Option<Mapping> {
-        Some(
+        Some(Mapping::new(
             Regex::new(&format!(r"(?m){name} map:\r\n([\s\d]+)"))
                 .ok()?
                 .captures(input)?
@@ -64,53 +60,39 @@ impl Almanac {
                 .flat_map(|line| {
                     match line
                         .split_ascii_whitespace()
-                        .map(|str| str.parse::<u32>().unwrap())
-                        .collect::<Vec<u32>>()[..]
+                        .map(|str| str.parse::<u64>().unwrap())
+                        .collect::<Vec<u64>>()[..]
                     {
                         [fst, snd, thrd] => Some((fst, snd, thrd)),
                         _ => None,
                     }
                 })
-                .collect(),
-        )
+                .collect::<HashSet<(u64, u64, u64)>>(),
+        ))
     }
 
-    fn mapping_to_hashmap(set: Mapping) -> HashMap<u32, u32> {
-        set.iter().fold(HashMap::new(), |mut acc, row| {
-            acc.extend(Self::mapping_row_to_hashmap(*row));
-            acc
-        })
-    }
-
-    fn mapping_row_to_hashmap(row: (u32, u32, u32)) -> HashMap<u32, u32> {
-        let keys = row.1..;
-        let values = row.0..;
-        HashMap::from_iter(zip(keys, values).take(row.2 as usize))
-    }
-
-    fn parse_to_hashmap(input: &str, name: &str) -> Option<HashMap<u32, u32>> {
-        Some(Self::mapping_to_hashmap(Self::parse_mapping(input, name)?))
-    }
-
-    fn hashmap_value(key: &u32, hashmap: &HashMap<u32, u32>) -> u32 {
-        *hashmap.get(key).unwrap_or(key)
-    }
-
-    fn seed_location(&self, seed: u32) -> u32 {
-        vec![
-            &self.seed_to_soil,
-            &self.soil_to_fertilizer,
-            &self.fertilizer_to_water,
-            &self.water_to_light,
-            &self.light_to_temperature,
-            &self.temperature_to_humidity,
-            &self.humidity_to_location,
+    fn parse_mappings(input: &str) -> Option<Vec<Mapping>> {
+        [
+            "seed-to-soil",
+            "soil-to-fertilizer",
+            "fertilizer-to-water",
+            "water-to-light",
+            "light-to-temperature",
+            "temperature-to-humidity",
+            "humidity-to-location",
         ]
-        .into_iter()
-        .fold(seed, |acc, el| Self::hashmap_value(&acc, el))
+        .iter()
+        .map(|name| Self::parse_mapping(input, name))
+        .collect()
     }
 
-    pub fn seed_locations(&self) -> Vec<u32> {
+    fn seed_location(&self, seed: u64) -> u64 {
+        self.mappings
+            .iter()
+            .fold(seed, |acc, mapping| mapping.get(acc))
+    }
+
+    pub fn seed_locations(&self) -> Vec<u64> {
         self.seeds
             .iter()
             .map(|seed| self.seed_location(*seed))
@@ -122,53 +104,45 @@ impl Almanac {
 mod test {
     use std::{collections::HashSet, fs};
 
-    use crate::almanac::{Almanac, Mapping};
+    use crate::almanac::{self, Almanac, Mapping};
 
     fn example() -> String {
         fs::read_to_string("./resources/example.txt").expect("to read file")
     }
 
-    fn almanac() -> Almanac {
-        Almanac::new(&example()).unwrap()
-    }
     #[test]
     fn test_parse_seeds() {
-        let expected: HashSet<u32> = vec![79, 14, 55, 13].into_iter().collect();
+        let expected: HashSet<u64> = vec![79, 14, 55, 13].into_iter().collect();
         assert_eq!(Some(expected), Almanac::parse_seeds(&example()));
     }
 
     #[test]
     fn test_parse_mapping_1() {
-        let expected: Mapping = HashSet::from([(50, 98, 2), (52, 50, 48)]);
+        let expected: Mapping = almanac::Mapping(HashSet::from([(50, 98, 2), (52, 50, 48)]));
         let actual = Almanac::parse_mapping(&example(), "seed-to-soil");
         assert_eq!(Some(expected), actual);
     }
 
     #[test]
     fn test_parse_mapping_2() {
-        let expected: Mapping = HashSet::from([(0, 15, 37), (37, 52, 2), (39, 0, 15)]);
+        let expected: Mapping =
+            almanac::Mapping(HashSet::from([(0, 15, 37), (37, 52, 2), (39, 0, 15)]));
         let actual = Almanac::parse_mapping(&example(), "soil-to-fertilizer");
         assert_eq!(Some(expected), actual);
     }
 
     #[test]
     fn test_hashmap_value_mapped() {
-        let almanac = almanac();
+        let mapping: Mapping = almanac::Mapping(HashSet::from([(50, 98, 2), (52, 50, 48)]));
         let input = 79;
         let expected = 81;
-        assert_eq!(
-            Almanac::hashmap_value(&input, &almanac.seed_to_soil),
-            expected
-        );
+        assert_eq!(mapping.get(input), expected);
     }
     #[test]
     fn test_hashmap_value_unmapped() {
-        let almanac = almanac();
+        let mapping: Mapping = almanac::Mapping(HashSet::from([(50, 98, 2), (52, 50, 48)]));
         let input = 13;
         let expected = 13;
-        assert_eq!(
-            Almanac::hashmap_value(&input, &almanac.seed_to_soil),
-            expected
-        );
+        assert_eq!(mapping.get(input), expected);
     }
 }
--- 05/src/main.rs
+++ 05/src/main.rs
@@ -9,7 +9,7 @@ fn solve_for_file(file_name: &str) {
     let contents = fs::read_to_string(path).expect("to read file");
     let almanac = Almanac::new(&contents).unwrap();
     let seed_locations = almanac.seed_locations();
-    let lowest_seed_location = seed_locations.iter().min();
+    let lowest_seed_location = seed_locations.iter().min().unwrap();
     dbg!(&seed_locations);
     dbg!(&lowest_seed_location);
 }
