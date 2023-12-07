8e6f0350e6587c6ebb608948fd08bb958415f376
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-12-07 15:11:31 +0100

    07 init

---
+++ 07/index.js
@@ -0,0 +1,9 @@
+//@ts-check
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(7, 8));
+
+module.exports = add;
---
+++ 07/index.test.js
@@ -0,0 +1,4 @@
+const add = require("./index");
+test("add", () => {
+  expect(add(1, 2)).toBe(3);
+});
