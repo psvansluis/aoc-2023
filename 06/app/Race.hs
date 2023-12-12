e24bbcc1fe9920accfb1c0c27581abf7ef938218
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-12-12 17:17:23 +0100

    refactor 06

--- 06/app/Race.hs
+++ 06/app/Race.hs
@@ -14,35 +14,26 @@ data Race = Race {time :: Int, distance :: Int} deriving (Show)
 parseRaces :: String -> [Race]
 parseRaces str = map toRace pairs
     where
-        [timeLine, distanceLine] = lines str
-        times = mapMaybe readMaybe $ words timeLine
-        distances = mapMaybe readMaybe (words distanceLine)
+        [times, distances] = map readLine $ lines str
+        readLine line = mapMaybe readMaybe $ words line
         pairs = zip times distances
         toRace (time, distance) = Race {time, distance}
 
 parseRace :: String -> Race
 parseRace str = Race {time, distance}
     where
-        [time, distance] = map bigNum $ lines str
+        (time : distance : _) = map bigNum $ lines str
         bigNum line = read $ filter isDigit line
 
 getDistance :: Int -> Int -> Int
 getDistance time speed = (time - speed) * speed
-
-minSpeedToBeat :: Race -> Int
-minSpeedToBeat = firstSpeedToBeat [1..]
-
-maxSpeedToBeat :: Race -> Int
-maxSpeedToBeat race = firstSpeedToBeat (map (\x -> (time race) - x) [1..]) race
 
 firstSpeedToBeat :: [Int] -> Race ->  Int
 firstSpeedToBeat iter Race {time, distance}  = fromJust $ find beats iter
     where beats speed = getDistance time speed > distance
 
 countWaysToBeat :: Race -> Int
-countWaysToBeat race
-    | max < min = 0
-    | otherwise = max - min + 1
+countWaysToBeat race = max 0 (maxSpeed - minSpeed + 1)
     where
-        min = minSpeedToBeat race
-        max = maxSpeedToBeat race
+        minSpeed = firstSpeedToBeat [1..] race
+        maxSpeed = firstSpeedToBeat [time race, time race - 1 ..] race
